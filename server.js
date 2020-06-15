19a2ee6b336d46125dee357e3853225444003a46
barywhyte <seunsamore@gmail.com>
2020-06-15 13:37:57 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http')
+const server = http.createServer((req, res) =>{
+    console.log('New Connection')
+    res.end('Hello Seun Ore')
+})
+
+const PORT = process.env.PORT || 8080
+server.listen(PORT, () => console.log('Listening'))
